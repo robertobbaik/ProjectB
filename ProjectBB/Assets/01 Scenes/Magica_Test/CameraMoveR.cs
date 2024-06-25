b2c3035b9e20852cfd942cd596f3f18a578f52b6
mandoo089 <mandoo089@naver.com>
2024-06-25 14:18:08 +0900

    무빙 테스트 씬
    회사에서 쓰던 무빙 테스트 씬 넣어봄

---
+++ ProjectBB/Assets/01 Scenes/Magica_Test/CameraMoveR.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMoveR : MonoBehaviour
+{
+	//
+	// VARIABLES
+	//
+	
+	public float turnSpeed = 2.0f;		// Speed of camera turning when mouse moves in along an axis
+	public float panSpeed = 0.15f;		// Speed of the camera when being panned
+
+	
+	private Vector3 mouseOrigin;	// Position of cursor when mouse dragging starts
+	private bool isPanning;		// Is the camera being panned?
+	private bool isRotating;	// Is the camera being rotated?
+
+	
+	
+	public float m_zoomSpeed = 4f;
+	public float m_zoomMax = 0f;
+	public float m_zoomMin = 15f;
+	
+	
+	//CameraZoom
+	void CameraZoom()
+	{
+		float t_zoomDirection = Input.GetAxis("Mouse ScrollWheel");
+		
+		if(transform.position.y <= m_zoomMax && t_zoomDirection>0)
+			return;
+			
+		if(transform.position.y >= m_zoomMin && t_zoomDirection<0)
+			return;
+			
+		transform.position += transform.forward*t_zoomDirection*m_zoomSpeed;
+		
+	}
+	
+	
+	void CamreaZ()
+	{
+		transform.position = new Vector3(-0.07f,1.57f,3.32f);
+		transform.rotation = Quaternion.Euler (new Vector3(6.53f,179.45f,0f));
+		
+	}
+	
+	void CamreaX()
+	{
+		transform.position = new Vector3(-4.16f,1.38f,0.08f);
+		transform.rotation = Quaternion.Euler (new Vector3(6.53f,95.55f,0f));
+		
+	}
+	
+	void CamreaC()
+	{
+		transform.position = new Vector3(-0.56f,1.76f,-2.86f);
+		transform.rotation = Quaternion.Euler (new Vector3(11.51f,17.33f,0f));
+		
+	}
+	
+	void CamreaV()
+	{
+		transform.position = new Vector3(4.15f,1.76f,0.72f);
+		transform.rotation = Quaternion.Euler (new Vector3(6.53f,263.54f,0f));
+		
+	}
+	
+	
+	//
+	// UPDATE
+	//
+	
+
+	
+	void Update () 
+	{
+		
+		CameraZoom();
+
+		// 카메라 위치
+		if (Input.GetKey(KeyCode.Z))
+		{
+			CamreaZ();
+		}
+		if (Input.GetKey(KeyCode.X))
+		{
+			CamreaX();
+		}
+		if (Input.GetKey(KeyCode.C))
+		{
+			CamreaC();
+		}
+		if (Input.GetKey(KeyCode.V))
+		{
+			CamreaV();
+		}
+
+
+
+		// Get the left mouse button
+		if(Input.GetMouseButtonDown(1))
+		{
+			// Get mouse origin
+			mouseOrigin = Input.mousePosition;
+			isRotating = true;
+		}
+		
+		// Get the middle mouse button
+		if(Input.GetMouseButtonDown(2))
+		{
+			// Get mouse origin
+			mouseOrigin = Input.mousePosition;
+			isPanning = true;
+		}
+		
+
+		// Disable movements on button release
+		if (!Input.GetMouseButton(1)) isRotating=false;
+		if (!Input.GetMouseButton(2)) isPanning=false;
+
+		
+		// Rotate camera along X and Y axis
+		if (isRotating)
+		{
+			Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - mouseOrigin);
+
+			transform.RotateAround(transform.position, transform.right, -pos.y * turnSpeed);
+			transform.RotateAround(transform.position, Vector3.up, pos.x * turnSpeed);
+		}
+		
+		// Move the camera on it's XY plane
+		if (isPanning)
+		{
+			Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - mouseOrigin);
+
+			Vector3 move = new Vector3(-pos.x * panSpeed, -pos.y * panSpeed, 0);
+			transform.Translate(move, Space.Self);
+		}
+		
+
+	}
+}
+
+
+
---
+++ ProjectBB/Assets/01 Scenes/Magica_Test/MagicaCloth_Test.cs
@@ -0,0 +1,274 @@
+﻿#if !OUT_GAME_ONLY
+using F4.Manager;
+using F4.Object;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FacialController : MonoBehaviour
+{
+	public FaceController faceController;
+
+	private void Awake()
+    {
+		faceController = gameObject.GetComponent<FaceController>();
+
+	}
+	protected void OnAnimEvent(string eventKey)
+	{
+		
+
+		switch (eventKey)
+		{
+			case "step_l":
+				{
+				}
+				break;
+			case "step_r":
+				{
+				}
+				break;
+			case "eyeclose":
+			case "angry":
+			case "fury":
+			case "surprise":
+			case "pain":
+			case "smile":
+				var controller = faceController;
+				if (controller)
+				{
+					controller.Play(eventKey);
+				}
+				break;
+
+		}
+	}
+	protected void FaceEvent(string eventKey)
+	{
+
+
+		switch (eventKey)
+		{
+			case "step_l":
+				{
+				}
+				break;
+			case "step_r":
+				{
+				}
+				break;
+			case "eyeclose":
+			case "angry":
+			case "fury":
+			case "surprise":
+			case "pain":
+			case "smile":
+				var controller = faceController;
+				if (controller)
+				{
+					controller.Play(eventKey);
+				}
+				break;
+
+		}
+	}
+}
+
+public class MagicaCloth_Test : MonoBehaviour
+{
+	Animator m_Animator;
+	//public float speed = 1f;
+
+	public float spinSpeed = 100f; // rotate speed
+	public RuntimeAnimatorController animcontroller;// animator add
+	public Object animdata;//= new AnimatorOverrideController(animcontroller);
+
+	//object
+
+	GameObject[] objt;
+	ManagerCharacter managerChar;
+	//Forward Direction
+	//bool ForwardY = true;
+
+
+	void _All() //animator motion call
+	{
+
+		for (int i = 0; i < objt.Length; i++)
+		{
+			//objt[i].transform.Rotate(0, Time.deltaTime * speed, 0, Space.Self); //rotate object
+			if (objt[i].transform.parent == null)
+				m_Animator = objt[i].GetComponent<Animator>();// object to ani
+
+		}
+
+	}
+
+
+	void _reset() // position reset
+	{
+		for (int i = 0; i < objt.Length; i++)
+		{
+			objt[i].transform.position = new Vector3(0, 0, 0); // reset position
+			objt[i].transform.rotation = Quaternion.Euler(0, 0, 0); // reset rotation
+		}
+	}
+
+
+	void _Ani() // animator add
+	{
+
+		for (int i = 0; i < objt.Length; i++)
+		{
+			Animator animator = objt[i].gameObject.GetComponent<Animator>();
+			var animatorOverrideController = new AnimatorOverrideController(animcontroller);
+			AnimationClip[] anims = Resources.LoadAll<AnimationClip>("MyModel");
+
+			animatorOverrideController.name = objt[i].gameObject.name + "_OvrAnimController";
+			//         for (int j = 0; j < animatorOverrideController.runtimeAnimatorController.animationClips.Length; j++)
+			//         {
+			//	Instantiate(animdata).name = "TEE";
+			//	//animdata.GetComponent<Animation>
+			//	//animatorOverrideController.runtimeAnimatorController.animationClips[j] = 
+
+			//}
+			animator.runtimeAnimatorController = animatorOverrideController;
+			//animator.gameObject.AddComponent<FacialController>();
+			if (animator.gameObject.GetComponent<PlayerController>() == null)
+				animator.gameObject.AddComponent<PlayerController>();
+			managerChar.SetCharacterFaceAnimation(animator.gameObject);
+		}
+
+	}
+
+	private void Awake()
+	{
+		managerChar = FindObjectOfType<ManagerCharacter>();
+
+		var objanimator = FindObjectsOfType<Animator>();// ("MagicaTest");
+		objt = new GameObject[objanimator.Length];
+		for (int i = 0; i < objanimator.Length; i++)
+		{
+			objt[i] = objanimator[i].gameObject;
+		}
+	}
+
+	void Start()
+	{
+		//Tag serch
+
+
+		_All();
+
+		_Ani();
+
+	}
+
+	void Update()
+	{
+
+
+
+		if (Input.GetKey(KeyCode.LeftArrow) == true)
+		{
+
+			objt = GameObject.FindGameObjectsWithTag("MagicaTest");
+			foreach (GameObject o in objt)
+				o.transform.Rotate(Vector3.up * spinSpeed * Time.deltaTime);
+		}
+
+		if (Input.GetKey(KeyCode.RightArrow) == true)
+		{
+
+			objt = GameObject.FindGameObjectsWithTag("MagicaTest");
+			foreach (GameObject o in objt)
+				o.transform.Rotate(-Vector3.up * spinSpeed * Time.deltaTime);
+		}
+
+
+		if (Input.GetKey(KeyCode.KeypadEnter))
+		{
+			_reset();
+		}
+
+
+		//Press the up arrow button to reset the trigger and set another one
+		if (Input.GetKey(KeyCode.Keypad0) || Input.GetKey(KeyCode.Alpha0))
+		{
+			//Send the message to the Animator to activate the trigger parameter named "Jump"
+			m_Animator.SetTrigger("00");
+		}
+
+		if (Input.GetKey(KeyCode.Keypad1) || Input.GetKey(KeyCode.Alpha1))
+		{
+			//Send the message to the Animator to activate the trigger parameter named "Crouch"
+			m_Animator.SetTrigger("01");
+		}
+
+		if (Input.GetKey(KeyCode.Keypad2) || Input.GetKey(KeyCode.Alpha2))
+		{
+			//Send the message to the Animator to activate the trigger parameter named "Crouch"
+			m_Animator.SetTrigger("02");
+		}
+
+		if (Input.GetKey(KeyCode.Keypad3) || Input.GetKey(KeyCode.Alpha3))
+		{
+			//Send the message to the Animator to activate the trigger parameter named "Crouch"
+			m_Animator.SetTrigger("03");
+		}
+
+		if (Input.GetKey(KeyCode.Keypad4) || Input.GetKey(KeyCode.Alpha4))
+		{
+			//Send the message to the Animator to activate the trigger parameter named "Crouch"
+			m_Animator.SetTrigger("04");
+		}
+
+		if (Input.GetKey(KeyCode.Keypad5) || Input.GetKey(KeyCode.Alpha5))
+		{
+			//Send the message to the Animator to activate the trigger parameter named "Crouch"
+			m_Animator.SetTrigger("05");
+		}
+
+		if (Input.GetKey(KeyCode.Keypad6) || Input.GetKey(KeyCode.Alpha6))
+		{
+			//Send the message to the Animator to activate the trigger parameter named "Crouch"
+			m_Animator.SetTrigger("06");
+		}
+
+		if (Input.GetKey(KeyCode.Keypad7) || Input.GetKey(KeyCode.Alpha7))
+		{
+			//Send the message to the Animator to activate the trigger parameter named "Crouch"
+			m_Animator.SetTrigger("07");
+		}
+
+		if (Input.GetKey(KeyCode.Keypad8) || Input.GetKey(KeyCode.Alpha8))
+		{
+			//Send the message to the Animator to activate the trigger parameter named "Crouch"
+			m_Animator.SetTrigger("08");
+		}
+
+		if (Input.GetKey(KeyCode.Keypad9) || Input.GetKey(KeyCode.Alpha9))
+		{
+			//Send the message to the Animator to activate the trigger parameter named "Crouch"
+			m_Animator.SetTrigger("09");
+		}
+
+		if (Input.GetKey(KeyCode.PageUp))
+		{
+			float time = Mathf.Min(Time.timeScale + 0.05f, 2);
+			Time.timeScale = time;
+		}
+
+		if (Input.GetKey(KeyCode.PageDown))
+		{
+			float time = Mathf.Max(Time.timeScale - 0.05f, 0.0f);
+			Time.timeScale = time;
+		}
+
+		if (Input.GetKey(KeyCode.Home))
+		{
+			Time.timeScale = 1;
+		}
+		}
+	}
+#endif
